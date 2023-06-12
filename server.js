560bdc5ac9fbcf06ea2c1a80112178f3f46ea5fa
Yaroslav Grogul <time7bass@gmail.com>
2023-06-12 17:58:22 +0200

    minor changes

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-    console.log('Server is running on port 3000');
+    console.log(`Server is running on port ${port}`);
 });
 
 const registrationRoute = require('./routes/registration');
