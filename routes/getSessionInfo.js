59938703c481f19c25b1a89ba6519f78d74fe6b2
Yaroslav Grogul <time7bass@gmail.com>
2023-06-13 19:50:28 +0200

    session/ endpoint debug

--- routes/getSessionInfo.js
+++ routes/getSessionInfo.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+  console.log(req.session);
   if (req.session.username) {
     res.status(200).json({ username: req.session.username });
   } else {
