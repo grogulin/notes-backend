3d19a16c008059223cb212ce9f11fe2ef2dc7654
Yaroslav Grogul <time7bass@gmail.com>
2023-06-14 19:04:10 +0200

    CORS updates

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(cors({
   credentials: true
 }));
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', allowedOrigins);
+  res.setHeader('Access-Control-Allow-Origin', 'http://freedevdom.mooo.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', true);
